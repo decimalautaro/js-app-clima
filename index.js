8bd3ece5bc93bdd220c4029c65f765b38de035cf
Lautaro Decima <decimalautaro@gmail.com>
2022-02-05 21:33:25 -0300

    creacion de style.css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let temperature = document.querySelector('.temp')
 let description = document.querySelector('.description')
 let loc = document.querySelector('.location')
 let icon = document.querySelector('.icon')
-
+const kelvin = 271.15
 
 
 window.addEventListener('load',()=> {
@@ -24,12 +24,17 @@ window.addEventListener('load',()=> {
 
             fetch(url_base)
             .then((response)=> {
-                console.log('Respuesta json');
+                console.log('Respuesta json')
                 return response.json()
             }) 
             .then((data) =>{
                 console.log ('Esta es la data')
                 console.log(data);
+
+                temperature.textContent = Math.floor(data.main.temp - kelvin ) + '°C'
+                description.textContent = data.weather[0].description
+                loc.textContent = data.name + ', ' + data.sys.country
+
             })
             
         }
